88cf8ea0b2f1c810242bc2b75765eb416ae8065e
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-30 16:45:59 -0400

    [dev] Adding admin seeder

---
+++ server/src/database/seeders/admin.seeder.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client'
+import { genSalt, hash } from 'bcrypt'
+
+const prisma = new PrismaClient() 
+
+const main = async () => {
+    const email = 'admin@farouk.ca'
+    const unhashedPassword = generatePassword(8)
+    const salt = await genSalt(8)
+    const password = await hash('password', salt)
+
+    await prisma.admin.upsert({
+        where: { email },
+        update: { salt, password },
+        create: {
+            email,
+            name: 'Super admin',
+            salt,
+            password
+        }
+    })
+
+    console.log(`Admin account ${email} created with password : ${unhashedPassword}`)
+}
+
+const generatePassword = (length: number): string => {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-?%$&()!'
+    
+    let password = ''
+
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n))
+    }
+    
+    return password
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+        process.exit(0)
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import adminAuthRouter from '#routes/admin/auth.routes.js'
 import adminUserRouter from '#routes/admin/user.routes.js'
 
 const router = Router() 
 
+router.use('/auth', adminAuthRouter)
 router.use('/users', adminUserRouter)
 
 export default router 
