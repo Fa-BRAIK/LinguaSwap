a9728c25d88aedf89a2045318be7f7e014ea82cd
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-30 12:29:08 -0400

    [dev] updating index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,15 +5,11 @@ import router from './routes/routes.js'
 
 dotenv.config()
 
-const start = async () => {
-  const port = config('app.port')
-  const app = express()
+const port = config('app.port')
+const app = express()
 
-  app.use(router)
+app.use(router)
 
-  app.listen(port, () => {
-    console.log(`Server side app listening on port ${port}`)
-  })
-}
-
-start()
+app.listen(port, () => {
+  console.log(`Server side app listening on port ${port}`)
+})
